9257f16f9bed36d58b9495adb5bc40674961639c
nitink2306 <nitinkrishna2306@gmail.com>
2025-01-22 11:21:37 -0500

    feat: add stripe payments

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,23 @@
 import { AuthFormProps, SIGN_IN_FORM, SIGN_UP_FORM } from "./forms"
 import { LANDING_PAGE_MENU, MenuProps } from "./menus"
+import {
+    CREATE_GROUP_PLACEHOLDER,
+    CreateGroupPlaceholderProps,
+} from "./placeholder"
+import { GROUP_LIST, GroupListProps } from "./slider"
 
 type GroupleConstantsProps = {
     landingPageMenu: MenuProps[]
     signUpForm: AuthFormProps[]
     signInForm: AuthFormProps[]
+    createGroupPlaceholder: CreateGroupPlaceholderProps[]
+    groupList: GroupListProps[]
 }
 
 export const GROUPLE_CONSTANTS: GroupleConstantsProps = {
     landingPageMenu: LANDING_PAGE_MENU,
     signUpForm: SIGN_UP_FORM,
     signInForm: SIGN_IN_FORM,
+    createGroupPlaceholder: CREATE_GROUP_PLACEHOLDER,
+    groupList: GROUP_LIST,
 }
