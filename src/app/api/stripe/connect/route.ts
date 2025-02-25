11048d0dd82b4f929c791560c1c76c947db09337
nitink2306 <nitinkrishna2306@gmail.com>
2025-02-25 01:40:29 -0500

    feat: modify stripe route

--- src/app/api/stripe/connect/route.ts
+++ src/app/api/stripe/connect/route.ts
@@ -5,7 +5,7 @@ import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   typescript: true,
-  apiVersion: "2024-06-20",
+  apiVersion: "2024-12-18.acacia",
 })
 
 export async function GET(req: NextRequest) {
